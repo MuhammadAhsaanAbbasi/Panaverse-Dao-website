cce2da309123a15966360c35032a80406ef46f42
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2023-12-22 01:16:50 +0500

    init commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-    <link rel="icon" href="/images/vercel.ico" />
+    <link rel="icon" href="/images/favicon.ico" />
     </head>
       <body className={inter.className}>
       <Navhead/>
