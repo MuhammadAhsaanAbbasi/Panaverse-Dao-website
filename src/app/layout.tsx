6089a08e204818783e571fabb80162541e65608b
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2023-12-23 08:41:02 +0500

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-    <link rel="icon" href="/images/favicon.ico" />
+    <link rel="icon" href="./public/favicon.ico" />
     </head>
       <body className={inter.className}>
       <Navhead/>
